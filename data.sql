d593717e9839d63c986909b059797eb1d62ba357
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-02 21:27:14 +0430

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -106,3 +106,11 @@ INSERT INTO
     weight_kg
   )
 VALUES ('Ditto', 'May 14, 2022', 4, TRUE, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+ ('Bob', 45),
+('Melody Pond', 77),
+ ('Dean Winchester', 14),
+('Jodie Whittaker', 38);

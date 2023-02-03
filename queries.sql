a94a34bb9db521e3342849b725d22958097ede21
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-03 13:49:47 +0430

    List all animals owned by Dean Winchester that haven't tried to escape.

--- queries.sql
+++ queries.sql
@@ -111,3 +111,8 @@ SELECT animals.name AS animal
 FROM animals
 JOIN species ON species_id = species.id
 WHERE species.name = 'Digimon' AND owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell');
+
+SELECT animals.name AS animal 
+FROM animals
+JOIN owners ON owner_id = owners.id
+WHERE animals.escape_attempts = 0 AND owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester');

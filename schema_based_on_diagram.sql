ae0d83484c89bb5ad60f61003233773eb4903285
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-07 20:02:08 +0430

    Create index for medical_history id

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -25,4 +25,5 @@ CREATE TABLE invoices(
 );
 
 CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
+CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
 CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);

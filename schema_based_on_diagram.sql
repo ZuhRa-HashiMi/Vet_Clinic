f36bfebaeb1438737927e789eec4cd4825259a07
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-02-07 16:46:38 +0430

    Create table mediacl_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,12 @@ CREATE TABLE patients(
 	name varchar(255),
 	date_of_birth DATE
 );
+
+-- create medical_hitories table
+CREATE TABLE medical_histories(
+	id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	admitted_at TIME,
+	patient_id INT ,
+	status varchar(255),
+    FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
